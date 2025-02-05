95c3c96ed589ece188dea5dc9acc88c6db08160d
Swapnali <Swapnali@WIN-S8PJN3QVBVB>
2025-02-05 12:52:06 +0530

    add logo in daynamic nav

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
   baseUrl: '',
   useHash: false,
-  apibaseUrl: 'http://localhost:55881/api/',
-  baseFileUrl: 'http://localhost:55881/CampaignConatiner/',
-  baseGSTFileUrl: 'http://localhost:55881/GSTFilesContainer/',
+  apibaseUrl: 'http://localhost:55891/api/',
+  baseFileUrl: 'http://localhost:55891/CampaignConatiner/',
+  baseGSTFileUrl: 'http://localhost:55891/GSTFilesContainer/',
   baseAgreementFileUrl: 'http://localhost:55881/AgreementFilesContainer/',
 };
